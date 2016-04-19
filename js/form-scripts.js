54ccb4ef2584981ca3e348aae9995e1edcd377d9
Matt Hummel <matt@mattrhummel.com>
2016-04-19 16:02:57 -0400

    Add Slider
    Added slider for testimonials

--- js/form-scripts.js
+++ js/form-scripts.js
@@ -1,13 +1,18 @@
-  $(document).foundation({
-    tab: {
-      callback : function (tab) {
-        console.log(tab);
-      }
-    }
-  });
- $('.tabs').on('toggled', function (event, tab) {
+$(document).ready(function(){
+	
+//Controls the continue button on the form tabs
+$('.tabs').on('toggled', function (event, tab) {
     console.log(tab);
   });
+//Makes tab advanced to step 2
 $('.next-tab').click(function() {
   $('.tabs li.active').next().children('a').click();
 });
+});
+$('.bxslider').bxSlider({
+  minSlides: 1,
+  maxSlides: 2,
+  slideMargin: 0
+}); 
+
+$(document).foundation();
